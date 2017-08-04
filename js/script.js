2c6bee3eccafa3094901783fe5097540fd637fec
Matt Woods <mattwoods21@gmail.com>
2017-08-03 19:22:59 -0500

    updated modal to fullscreen

--- js/script.js
+++ js/script.js
@@ -179,3 +179,23 @@ var addHabitButton = function(habit) {
       habitButton += '</div>'
       return habitButton;
 }
+
+// .modal-backdrop classes fullscreen
+
+$(".modal-transparent").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+  }, 0);
+});
+$(".modal-transparent").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-transparent");
+});
+
+$(".modal-fullscreen").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+});
+$(".modal-fullscreen").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
